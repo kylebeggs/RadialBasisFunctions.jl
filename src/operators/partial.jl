edacb6826619dcc196414f1719a9f5f212b53058
Kyle Beggs <beggskw@gmail.com>
2023-08-17 08:55:01 -0400

    add different centers - lap & partial constructors

--- src/operators/partial.jl
+++ src/operators/partial.jl
@@ -24,5 +24,20 @@ function partial(
     return RadialBasisOperator(ℒ, data, basis; k=k)
 end
 
+function partial(
+    data::AbstractVector{D},
+    centers::AbstractVector{D},
+    order::T,
+    dim::T,
+    basis::B=PHS(3; poly_deg=2);
+    k::T=autoselect_k(data, basis),
+) where {D<:AbstractArray,T<:Int,B<:AbstractRadialBasis}
+    f = let o = order, dim = dim
+        x -> ∂(x, o, dim)
+    end
+    ℒ = Partial(f, order, dim)
+    return RadialBasisOperator(ℒ, data, centers, basis; k=k)
+end
+
 # pretty printing
 print_op(op::Partial) = "∂ⁿ/∂xᵢ (n = $(op.order), i = $(op.dim))"
