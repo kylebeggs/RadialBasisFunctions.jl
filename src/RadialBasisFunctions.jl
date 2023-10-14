f04a8d4b3b4f02e4c01e72120734ca97e6cd966b
Kyle Beggs <beggskw@gmail.com>
2023-10-14 17:31:25 -0400

    add precompile tools

--- src/RadialBasisFunctions.jl
+++ src/RadialBasisFunctions.jl
@@ -57,4 +57,36 @@ export _build_weightmx, _build_collocation_matrix!, _build_rhs!, _build_stencil!
 #test
 export ∂test, ∂exponents, build_monomial_basis, pascals_triangle, monomial_recursive_list
 
+using PrecompileTools
+@setup_workload begin
+    f(x) = 1 + sin(4 * x[1]) + cos(3 * x[1]) + sin(2 * x[2])
+    @compile_workload begin
+        x = map(x -> SVector{2}(rand(2)), 1:100)
+        z = f.(x)
+
+        # partial
+        ∂ = partial(x, 1, 1)
+        ∂x = ∂(z)
+
+        # gradient
+        ∇ = gradient(x)
+        ∇z = ∇(z)
+
+        # laplacian
+        ∇² = laplacian(x)
+        ∇²z = ∇²(z)
+
+        # interpolation
+        interp = RadialBasisInterp(x, y)
+
+        # basis functions
+        imq = IMQ(1)
+        g = Gaussian(1)
+        phs1 = PHS(1; poly_deg=-1)
+        phs3 = PHS(3; poly_deg=0)
+        phs5 = PHS(5; poly_deg=1)
+        phs7 = PHS(7; poly_deg=2)
+    end
 end
+
+end # module
